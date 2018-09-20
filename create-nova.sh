ed888b9624b82e4b0f29fef00bcd19c5c854ac7e
codeitlikemiley <codeitlikemiley@gmail.com>
2018-09-20 11:17:54 +0800

    updated

--- create-nova.sh
+++ create-nova.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
-RED="\033[0;31m"
-NC="\033[0m"
+
 echo "What Is Your Package Name? (note: use CamelCase)"
 read package
 if [ -f $package ] ; then
-    echo "${RED}Opps You didnt Set A Package Name${NC}\n"
-    return;
+    echo "Opps You didnt Set A Package Name"
+    exit;
 fi
 
 echo "Whats Your Vendor Name?"
 read vendor
 if [ -f $vendor ] ; then
     vendor="$(echo "$(git config --global user.name)")"
-    echo "\n"
-    echo "Using Git Config Username as Default"
+    echo "Default Vendor Name: : Git Config Username"
+    echo $'\n'
 fi
 
 echo "What Is The Author's Name?"
 read author_name
 if [ -f $author_name ] ; then
     author_name="$(echo "$(whoami)" | sed 's/.*/\u&/')"
-    echo "Setting System User As Default Author's Name"
+    echo "Default Author's Name: System User"
+    echo $'\n'
 fi
 
 echo "What Is The Author's Username?"
 read author_username
 if [ -f $author_username ] ; then
     author_username="$(echo "$(git config --global user.name)")"
-    echo "Setting System User As Default Author's Username"
+    echo "Default Author's Username: Git Config Username"
+    echo $'\n'
 fi
 
 echo "Whats Your Author's Email?"
 read author_email
 if [ -f $author_email ] ; then
     author_email="$(echo "$(git config --global user.email)")"
-    echo "Using Git Config Email as Default Author's Email"
+    echo "Default Author's Email: Git Config Email"
+    echo $'\n'
 fi
 
 echo "Set Package Description?"
 read package_description
 if [ -f $package_description ] ; then
     package_description="A Laravel Nova Package"
-    echo "Using Default Description"
+    echo "Default Description: A Laravel Nova Package"
+    echo $'\n'
 fi
 namespace_vendor="$(echo "$(git config --global user.name)" | sed 's/.*/\u&/')"
 namespace_tool_name=$package
@@ -52,9 +55,13 @@ package_name=$(sed --expression 's/\([A-Z]\)/-\L\1/g' \
 git clone https://github.com/spatie/skeleton-nova-tool $package_name
 cd $package_name
 
+echo $'\n'
 rm -rf .git
+echo $'\n'
 git init
+echo $'\n'
 git add .
+echo $'\n'
 git commit -m "Initial Commit!"
 find $(pwd) -type f -print0 | xargs -0 sed -i "s/:author_name/$author_name/g"
 find $(pwd) -type f -print0 | xargs -0 sed -i "s/:vendor/$vendor/g"
@@ -65,4 +72,7 @@ find $(pwd) -type f -print0 | xargs -0 sed -i "s/:package_description/$package_d
 find $(pwd) -type f -print0 | xargs -0 sed -i "s/:namespace_vendor/$namespace_vendor/g"
 find $(pwd) -type f -print0 | xargs -0 sed -i "s/:namespace_tool_name/$namespace_tool_name/g"
 
-echo "${RED}LARAVEL NOVA PACKAGE GENERATED! RUN `composer install` and `yarn && yarn production`"
+echo $'\n'
+echo "LARAVEL NOVA PACKAGE GENERATED"
+echo '`composer install`'
+echo '`yarn && yarn production`'
